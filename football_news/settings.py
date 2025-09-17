bbe39af6d7f83159ff1678fa8897f74318768281
RayhanDerya <rayhanderya9@gmail.com>
2025-09-17 15:33:44 +0700

    Tutorial 3

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rayhan-derya-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/rayhan.derya/kitmansgallery"
+    'https://pbp.cs.ui.ac.id/rayhan.derya/footballnews'
 ]
 
 # Application definition
